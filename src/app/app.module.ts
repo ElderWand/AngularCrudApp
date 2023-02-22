20c590be849119f7a2d2758820e61c57c557e195
ElderWand <ucef.bel.mahi@gmail.com>
2023-02-22 04:08:40 +0100

    Setup API service (add post command)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -39,6 +40,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    HttpClientModule,
 
     NgToastModule,
     NgConfirmModule,
--- src/app/components/create-registration/create-registration.component.ts
+++ src/app/components/create-registration/create-registration.component.ts
@@ -1,5 +1,7 @@
+import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-create-registration',
@@ -24,7 +26,10 @@ importantList: string[] = [
 
 public registerForm!: FormGroup;
 
-constructor(private fb: FormBuilder) {
+constructor(
+  private fb: FormBuilder, 
+  private api: ApiService, 
+  private toastService: NgToastService) {
 
 }
 
@@ -37,6 +42,8 @@ ngOnInit(): void {
     phone: [''],
     weight: [''],
     height: [''],
+    bmi: [''],
+    bmiResult: [''],
     requireTrainer: [''],
     userGender: [''],
     userPackage: [''],
@@ -45,10 +52,41 @@ ngOnInit(): void {
     enquiryDate: ['']
   })
 
+  this.registerForm.controls['height'].valueChanges.subscribe(res => {
+    this.calculateBmi(res);
+  });
+
 }
 
 submit() {
-  console.log(this.registerForm.value);
+  this.api.postRegistredUser(this.registerForm.value)
+  .subscribe(res=>{
+    this.toastService.success({detail: "Success", summary: "Enquiry Added", duration: 3000});
+    this.registerForm.reset;
+  })
+}
+
+calculateBmi(heightValue: number) {
+
+  const weight = this.registerForm.value.weight;
+  const height = heightValue;
+  const bmi = weight / (height * 2);
+  this.registerForm.controls['bmi'].patchValue(bmi);
+
+  switch (true) {
+    case bmi < 18.5:
+      this.registerForm.controls['bmiResult'].patchValue("Underweight");
+      break;
+    case bmi >= 18.5 && bmi < 25:
+      this.registerForm.controls['bmiResult'].patchValue("Normal");
+      break;
+    case bmi >= 25 && bmi < 30:
+      this.registerForm.controls['bmiResult'].patchValue("Overweight");
+      break;
+    default:
+      this.registerForm.controls['bmiResult'].patchValue("Obese");
+      break;
+  }
 }
 
 }
